26efa7742d9c15c6150b37c8f22228e261d0065c
LucaGDn <98167956+LucaGDn@users.noreply.github.com>
2022-01-25 20:32:58 -0300

    Add files via upload

---
+++ app_proyectoFinal/forms.py
@@ -0,0 +1,25 @@
+from django.forms import DateField, Form, IntegerField, CharField, EmailField, FloatField
+
+class atleta_form(Form):
+    nombre = CharField()
+    apellido = CharField()
+    edad = IntegerField()
+    altura = FloatField()
+    peso = FloatField()
+    email = EmailField()
+    
+class rutinas_create(Form):
+
+    nombre = CharField()
+    fecha_inicio = DateField()
+    intensidad = CharField()
+    ejercicio_1 = CharField()
+    ejercicio_2 = CharField()
+    ejercicio_3 = CharField()
+    ejercicio_4 = CharField()
+    ejercicio_5 = CharField()
+    ejercicio_6 = CharField()
+    ejercicio_7 = CharField()
+    duracionPorEjercicio = IntegerField()
+    descansoEntreEjercicio = IntegerField()
+    rondas = IntegerField()
